198d671c3006733861bdcc7d1c2c2cecfd52f730
nao20010128nao <nao20010128@gmail.com>
2019-11-20 13:18:28 +0000

    最初

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import(
+    "github.com/greymd/ojichat/generator"
+    "syscall/js"
+)
+
+func ojichat(this js.Value, i []js.Value) (interface {}){
+    config := generator.Config{}
+    if len(i) >= 1 {
+        jsCfg := i[0]
+        targetName := jsCfg.Get("targetName")
+        emojiNum := jsCfg.Get("emojiNum")
+        punctuationLevel := jsCfg.Get("punctuationLevel")
+        if targetName.Type() == js.TypeString {
+            config.TargetName = targetName.String()
+        }
+        if emojiNum.Type() == js.TypeNumber {
+            config.EmojiNum = emojiNum.Int()
+        }
+        if punctuationLevel.Type() == js.TypeNumber {
+            config.PunctiuationLevel = punctuationLevel.Int()
+        }
+    }
+    result, err := generator.Start(config)
+    jsResult := js.Global().Get("Object").New()
+    jsResult.Set("result", result)
+    if err != nil {
+        jsResult.Set("error", err.Error())
+    }
+    return jsResult
+}
+
+func registerCallbacks() {
+    js.Global().Set("ojichat", js.FuncOf(ojichat))
+}
+
+func main(){
+    c := make(chan struct{}, 0)
+    registerCallbacks()
+    <-c
+}
